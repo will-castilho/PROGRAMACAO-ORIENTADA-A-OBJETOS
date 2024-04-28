c6a0fedf9882582513d5b70d9853170170f28e34
will-castilho <willian.cp@edu.udesc.br>
2024-04-28 15:53:00 -0300

    Classe Pessoa agora com atributos publicos

--- src/app/ProgramaPrincipal.java
+++ src/app/ProgramaPrincipal.java
@@ -2,9 +2,21 @@ package app;
 
 import domain.Pessoa;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 public class ProgramaPrincipal {
     public static void main(String[] args){
         Pessoa fulano = new Pessoa();
-        // System.out.println(fulano.nome);
+
+        fulano.nome = "Fulano da Silva";
+        fulano.endereco = "Rua dos programadores, 42";
+        fulano.nascimento = LocalDate.of(1990, 10, 13);
+        fulano.salario = new BigDecimal("1000.99");
+
+        System.out.println("Nome.....: " + fulano.nome);
+        System.out.println("Endereço.....: " + fulano.endereco);
+        System.out.println("Nascimento.....: " + fulano.nascimento);
+        System.out.println("Salário.....: " + fulano.salario);
     }
 }
--- src/domain/Pessoa.java
+++ src/domain/Pessoa.java
@@ -4,8 +4,8 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public class Pessoa {
-    String nome;
-    String endereco;
-    LocalDate nascimento;
-    BigDecimal salario;
+    public String nome;
+    public String endereco;
+    public LocalDate nascimento;
+    public BigDecimal salario;
 }

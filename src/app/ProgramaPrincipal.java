8978f65feaa5b5f2370124e3bde6cac1229e97e3
will-castilho <willian.cp@edu.udesc.br>
2024-04-28 15:41:11 -0300

    Classe Pessoa e falha em teste com classe em outro pacote

--- src/app/ProgramaPrincipal.java
+++ src/app/ProgramaPrincipal.java
@@ -1,4 +1,10 @@
 package app;
 
+import domain.Pessoa;
+
 public class ProgramaPrincipal {
+    public static void main(String[] args){
+        Pessoa fulano = new Pessoa();
+        // System.out.println(fulano.nome);
+    }
 }
